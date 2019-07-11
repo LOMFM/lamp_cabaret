16edf0968e404785f56c96ef530d22a2b90fd27d
LOMFM <brightdawn019@gmail.com>
2019-07-11 11:00:06 +0800

    git ignore reset

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -8,89 +8,11 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  constructor(){
 
-  user = {
-    email: "",
-    password: ""
   }
 
-  error = "";
-
-  globalResponse : any;
-  forgotPassword = false;
-  firstStep = true;
-
-  forgotRequestFormError = "";
-  resetPasswordFormError = "";
-
-  forgot_email = "";
-  reset_code = "";
-  new_password = "";
-  confirm_password = "";
-
-
-  constructor( 
-    private router: Router,
-  ) { }
-
-  ngOnInit() {
-  }
-
-  login() {
+  ngOnInit(){
     
-  }
-
-  socialSignIn(socialPlatform: string){
-    
-  }
-
-  loginTrigger( evt ){
-    if( evt.keyCode == 13 ){
-      this.login();
-    }
-  }
-
-  openForgotPassword() {
-    this.forgotPassword = true
-  }
-
-  toStep2() {
-    this.firstStep = false;
-  }
-
-  toStep1() {
-    this.firstStep = true;
-  }
-
-  checkClosePopup(evt){
-    if(evt.target.classList.contains("popup-scroll")){
-      this.forgot_email = ""
-      this.reset_code = ""
-      this.forgot_email = ""
-      this.new_password = ""
-      this.firstStep = true;
-      this.forgotPassword = false;
-      this.firstStep = true;
-    }
-  }
-
-  resetPasswordRequest() {
-    
-  }
-
-  resetPassword() {
-    if ( this.new_password != this.confirm_password ){
-      this.resetPasswordFormError = "Please confirm the password."
-      return;
-    }
-    if ( this.forgot_email == "" ) {
-      this.resetPasswordFormError = "Please only enter your email in before step."
-      return;
-    }
-    let resetPasswordData = {
-      code: this.reset_code,
-      email: this.forgot_email,
-      password: this.new_password
-    }
   }
 }
